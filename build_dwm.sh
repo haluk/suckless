56c40678a4290709206517acbc98855327279812
Haluk Dogan <haluk.dogan@huskers.unl.edu>
2020-09-02 00:16:43 -0500

    build scripts and patches

---
+++ build_dwm.sh
@@ -0,0 +1,8 @@
+git merge config -m config
+git merge dwmconfig -m dwmconfig
+git merge actualfullscreen -m actualfullscreen
+git merge alwayscenter -m alwayscenter
+git merge fullgaps -m fullgaps
+git merge hide-vacant-tags -m "hide vacant tags"
+git merge removeborder -m removeborder
+git merge notitle -m notitle
---
+++ clean_repo.sh
@@ -0,0 +1,1 @@
+make clean && rm -f config.h && git reset --hard origin/master
