25b26520dea6e2cbc0a0c621d29a1dfc0a2af9fc
Haluk Dogan <haluk.dogan@huskers.unl.edu>
2021-02-18 16:05:38 -0600

    updated st theme

--- st/config.def.h
+++ st/config.def.h
@@ -1,5 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-
+#include "tempus_future.h"
 /*
  * appearance
  *
@@ -112,73 +112,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.9;
-
-/* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-    /* 8 normal colors */
-    [0] = "#232323",
-    [1] = "#d2813d",
-    [2] = "#8c9e3d",
-    [3] = "#b1942b",
-    [4] = "#6e9cb0",
-    [5] = "#b58d88",
-    [6] = "#6da280",
-    [7] = "#949d9f",
-
-    /* 8 bright colors */
-    [8] = "#312e30",
-    [9] = "#d0913d",
-    [10] = "#96a42d",
-    [11] = "#a8a030",
-    [12] = "#8e9cc0",
-    [13] = "#d58888",
-    [14] = "#7aa880",
-    [15] = "#aeadaf",
-
-    /* more colors can be added after 255 to use with DefaultXX */
-
-    /*
-     * PRO TIP: this is good when trying to dynamically change from a
-     * dark to a light theme or vice versa using ANSI escape sequences.
-     *
-     * Scenario:
-     *
-     * If on a dark theme we set defaultbg = 0 (with 15 being the
-     * defaultfg), then changing the value of '0' to what would be
-     * appropriate for a light theme (e.g. #ffffff) would turn the
-     * background white, when what we really wanted was to alter the
-     * foreground.
-     * That is because the assumption for the design of the Tempus
-     * themes is that colour codes represent colours.  So if a light
-     * theme needs a black foregound, it must use color0.  Whereas
-     * others default to defining the value of color0 as equivalent to
-     * background regardless of the colour value (and color15 as the
-     * foreground in the same way).  I think this is semantically
-     * incorrect and might cause problems.
-     *
-     * For working examples, see my dotfiles, specifically the
-     * 'own_script_update_running_terminals' under the "bin" directory:
-     * https://gitlab.com/protesilaos/dotfiles.  Note that this script
-     * is invoked from the 'own_script_update_environment_theme', which
-     * is typically interfaced via the 'tempusmenu'.
-     */
-    [256] = "#232323",
-    [257] = "#aeadaf",
-    [258] = "#da70d6",
-    [259] = "#232323",
-};
-
-/* Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultbg = 256;
-unsigned int defaultfg = 257;
-unsigned int defaultcs = 258;
-static unsigned int defaultrcs = 259;
-static unsigned int mousebg = 258;
-static unsigned int mousefg = 259;
-
+float alpha = 1;
 
 /*
  * Default shape of cursor
@@ -262,10 +196,9 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      clippaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,         {.i =  0} },
 	{ MODKEY,               XK_l,           copyurl,         {.i =  0} },
-	{ MODKEY,               XK_o,           opencopied,      {.v = "xdg-open"} },
 	{ TERMMOD,              XK_Return,      newterm,         {.i =  0} },
-	{ TERMMOD,              XK_U,           externalpipe,    { .v = openurlcmd } },
 	{ TERMMOD,              XK_Escape,      keyboard_select, { 0 } },
+	{ TERMMOD,              XK_I,           iso14755,        {.i =  0} },
 	{ TERMMOD,              XK_X,           invert,          { 0 } },
 };
 
@@ -306,7 +239,7 @@ static uint ignoremod = Mod2Mask|XK_SWITCH_MOD;
  * If no match is found, regular selection is used.
  */
 static uint selmasks[] = {
-	[SEL_RECTANGULAR] = Mod1Mask,
+	[SEL_RECTANGULAR] = Mod4Mask,
 };
 
 /*
@@ -317,9 +250,3 @@ static char ascii_printable[] =
 	" !\"#$%&'()*+,-./0123456789:;<=>?"
 	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
 	"`abcdefghijklmnopqrstuvwxyz{|}~";
-
-/*
- * plumb_cmd is run on mouse button 3 click, with argument set to
- * current selection and with cwd set to the cwd of the active shell
- */
-static char *plumb_cmd = "plumb";
